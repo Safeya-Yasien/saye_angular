eb6fc5802151af9e714720b7d429bd9496d85167
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-11 16:05:04 +0300

    add route pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,44 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { VolunteerOpportunityComponent } from './volunteer-opportunity/volunteer-opportunity.component';
+import { DonateComponent } from './donate/donate.component';
+import { ContactComponent } from './contact/contact.component';
+import { JoinusComponent } from './joinus/joinus.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent,
+  },
+  {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'volunteer-opportunity',
+    component: VolunteerOpportunityComponent,
+  },
+  {
+    path: 'donate',
+    component: DonateComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'joinus',
+    component: JoinusComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
