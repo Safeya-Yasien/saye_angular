80dd47e573343e540d5229c222d53087e53475ef
Meryhanwahed <meroowahed3@gmail.com>
2024-10-12 23:39:01 +0300

    faq ts

--- src/app/joinus/faq/faq.component.ts
+++ src/app/joinus/faq/faq.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
-export class FaqComponent {
+export class FaqComponent implements AfterViewInit {
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    document.querySelectorAll('.faq-question').forEach((question) => {
+      // Use an arrow function to retain the proper 'this' context
+      question.addEventListener('click', () => {
+        const answer = question.nextElementSibling as HTMLElement;
+        
+        // Toggle between showing and hiding the active panel
+        if (answer.classList.contains('collapse')) {
+          answer.classList.remove('collapse'); // Show the answer
+        } else {
+          // Hide all other answers
+          document.querySelectorAll('.faq-answer').forEach((item) => {
+            item.classList.add('collapse'); // Collapse all answers
+          });
+          answer.classList.remove('collapse'); // Show the clicked answer
+        }
+      });
+    });
+  }
 }
