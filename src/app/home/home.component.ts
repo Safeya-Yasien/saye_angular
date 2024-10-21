8c935d65e0f208d2a56e79c51a8d0eb75b8c5379
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-22 01:18:41 +0300

    delete slick

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { HeroComponent } from './hero/hero.component';
 import { CounterAreaComponent } from './counter-area/counter-area.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
-import { SlickComponent } from './slick/slick.component';
 import { VolunteerRegistrationComponent } from './volunteer-registration/volunteer-registration.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { EventsComponent } from './events/events.component';
@@ -17,7 +16,6 @@ import { EventsComponent } from './events/events.component';
     EventsComponent,
     CounterAreaComponent,
     HowItWorksComponent,
-    SlickComponent,
     TestimonialsComponent,
     VolunteerRegistrationComponent,
   ],
